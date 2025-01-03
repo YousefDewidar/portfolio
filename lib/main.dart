4702f300774694976502119b68528e32481052e9
yousefdewidar <ymahmoud1213@gmail.com>
2025-01-03 06:44:01 +0200

    Add TODO comments for future project enhancements

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_portfolio/features/home/home_view.dart';
 
+/* 
+TODO: 1. Create a new project in your Supabase.
+TODO: 2. Edit responsive in home
+TODO: 3. Split images
+TODO: 4. Create about me, contact me , skills , projects, services and resume pages
+TODO: 5. Add a view for projects and contact me
+TODO: 6. Create a Chatbot for the website
+*/
+
 void main() {
   runApp(const MyApp());
 }
